aa0b5aaf569c03c529da0651de3a3a27e585b565
eliaa04 <123811185+eliaa04@users.noreply.github.com>
2024-03-16 16:38:20 +0200

    Add files via upload

---
+++ PhoneBook/Program.cs
@@ -0,0 +1,143 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+
+namespace Телефонен_указател
+{
+    internal class Program
+    {
+        //const int Count = 100;
+        class Node
+        {
+            public long Next;
+            public long Contact;
+        }
+        class StreamLinkedList
+        {
+            FileStream fileStream;
+            long firstNode;
+            BinaryWriter bw;
+            BinaryReader br;
+            
+            public StreamLinkedList(string filePath)
+            {
+                FileStream fileStream = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+                bw = new BinaryWriter(fileStream, System.Text.Encoding.ASCII);
+                br = new BinaryReader(fileStream, System.Text.Encoding.ASCII);
+                if (fileStream.Length == 0)
+                    bw.Write(-1L);
+                else
+                    firstNode = br.ReadInt64();
+            }
+            public void Add(string name, string num)
+            {
+                long newNode = fileStream.Length;
+                // Kazvame che shte dobavqme nai otzad
+                fileStream.Position = newNode; 
+                //Добавя новия възел и го насочва към първия досега
+                bw.Write(firstNode);
+                bw.Write(name);
+                bw.Write(num);
+                //променя първия да сочи към новия
+                fileStream.Position = 0;
+                bw.Write(newNode);
+                firstNode = newNode;
+            }
+            public string Find (string name)
+            {
+                fileStream.Position = 0;
+                var node = br.ReadInt64();
+
+                
+                while (node != -1)
+                {
+                    fileStream.Position = node;
+                    var nodeNext = br.ReadInt64();
+                    var contactName = br.ReadString();
+
+                    if (contactName == name)
+                    {
+                        return br.ReadString();
+                    }
+                    else
+                        node = nodeNext;
+                }
+                return null;
+
+            }
+            public void Dispose()
+            {
+                bw.Dispose();
+                br.Dispose();
+                fileStream.Dispose();
+            }
+        }
+        
+        static StreamLinkedList streamLinkedList;
+
+        /*static void AddContact(string name, string num)
+        {
+            var newNode = new Node
+            {
+                Contact = new Contact
+                {
+                    Name = name,
+                    Num = num
+                },
+                Next = null
+            };
+            newNode.Next = firstNode;
+            firstNode = newNode;
+            
+        }
+        static string Find (string name)
+        {
+            var node = firstNode;
+            while(node != null && node.Contact.Name != name)
+            {
+                node = node.Next;
+            }
+
+            return node ?.Contact.Num;
+        }*/
+
+        static void Main(string[] args)
+        {
+            streamLinkedList = new StreamLinkedList("contacts");
+            while(true)
+            {
+                Console.Clear();
+                Console.WriteLine("a - add; f - find, q - quit");
+                var command = Console.ReadKey().KeyChar;
+                Console.Clear();
+                switch (command)
+                {
+                    case 'a':
+                        {
+                            Console.Write("Enter name: ");
+                            var name = Console.ReadLine();
+                            Console.Write("Enter number: ");
+                            var num = Console.ReadLine();
+                            streamLinkedList.Add(name, num);
+                        }
+                        break;
+                    case 'f':
+                        {
+                            Console.Write("Enter name: ");
+                            var name = Console.ReadLine();
+                            var num = streamLinkedList.Find(name);
+                            if (num != null)
+                                Console.WriteLine($"Num: {num}");
+                            else
+                                Console.WriteLine("Not found!");
+                        }
+                        Console.WriteLine();
+                        break;
+                    case 'q':
+                        return;
+                }
+            }
+        }
+    }
+}
---
+++ Tree/Program.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace familyTree
+{
+    class Program
+    {
+        class TreeNode
+        {
+            public char Name;
+            public TreeNode Mother;
+            public TreeNode Father;
+        }
+
+        static string ToString(TreeNode node)
+        {
+            if (node.Father != null && node.Mother != null)
+                return ToString(node.Father) + ToString(node.Mother) + char.ToUpper(node.Name);
+            else
+                return $"{char.ToLower(node.Name)}";
+        }
+
+        static TreeNode Parse(string text)
+        {
+            var stack = new Stack<TreeNode>();
+
+            foreach (var c in text)
+            {
+                var newNode = new TreeNode { Name = char.ToLower(c) };
+                if (char.IsUpper(c))
+                {
+                    newNode.Mother = stack.Pop();
+                    newNode.Father = stack.Pop();
+                }
+                stack.Push(newNode);
+            }
+
+            if (stack.Count != 1)
+                throw new Exception("Not correct text!");
+
+            return stack.Pop();
+        }
+
+        static void Print(List<TreeNode> level, int depth)
+        {
+            if (depth == 0)
+                return;
+
+            var nextLevel = new List<TreeNode>();
+
+            var spaces = Math.Pow(2, depth) - 1; //  <==>   var spaces = (1 << depth) - 1;
+
+            foreach (var n in level)
+            {
+                for (int s = 0; s < spaces; s++)
+                    Console.Write(' ');
+
+                Console.Write(n == null ? ' ' : n.Name);
+
+                for (int s = 0; s < spaces; s++)
+                    Console.Write(' ');
+
+                Console.Write(' ');
+
+                nextLevel.Add(n == null ? null : n.Father);
+                // и двете са еднакви
+                nextLevel.Add(n?.Mother);
+            }
+            Console.WriteLine();
+
+            Print(nextLevel, depth - 1);
+        }
+
+        static int Depth(List<TreeNode> level)
+        {
+            var nextLevel = new List<TreeNode>();
+
+            foreach (var n in level)
+            {
+                if (n.Father != null)
+                    nextLevel.Add(n.Father);
+
+                if (n.Mother != null)
+                    nextLevel.Add(n.Mother);
+            }
+
+            if (level.Count == 0)
+                return 0;
+
+            return Depth(nextLevel) + 1;
+
+            //  <==>   return nextLevel.Count == 0 ? 0 : Depth(nextLevel) + 1;
+        }
+
+        static TreeNode Find(TreeNode node, char name)
+        {
+            if (node == null)
+                return null;
+
+            if (node.Name == name)
+                return node;
+
+            //var result = Find(node.Father, name) ?? Find(node.Mother, name); - друг вариант на if-а  <==>
+
+            var result = Find(node.Father, name);
+            if (result == null)
+                result = Find(node.Mother, name);
+
+            return result;
+        }
+
+        static void AddParents(TreeNode root, char child, char father, char mother)
+        {
+            var node = Find(root, child);
+            if (node != null)
+            {
+                node.Father = new TreeNode { Name = father };
+                node.Mother = new TreeNode { Name = mother };
+            }
+        }
+        static void Main(string[] args)
+        {
+            TreeNode root;
+
+            if (File.Exists("tree"))
+            {
+                var savedTreeText = File.ReadAllText("tree");
+                root = Parse(savedTreeText);
+            }
+            else
+                root = new TreeNode { Name = 'a' };
+
+            while (true)
+            {
+                Console.Clear();
+
+                var depth = Depth(new List<TreeNode>() { root });
+                Print(new List<TreeNode>() { root }, depth);
+
+                Console.WriteLine();
+                Console.WriteLine(ToString(root));
+                Console.WriteLine();
+
+                Console.WriteLine();
+                Console.WriteLine("a - Add parents; f - Find parents; q - Quit");
+                var command = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                switch (command)
+                {
+                    case 'a':
+                        Console.Write("Child: ");
+                        var child = Console.ReadKey().KeyChar;
+                        Console.WriteLine();
+
+                        Console.Write("Father: ");
+                        var father = Console.ReadKey().KeyChar;
+                        Console.WriteLine();
+
+                        Console.Write("Mother: ");
+                        var mother = Console.ReadKey().KeyChar;
+                        Console.WriteLine();
+
+                        AddParents(root, child, father, mother);
+
+                        break;
+                    case 'f':
+                        Console.Write("Name: ");
+                        var name = Console.ReadKey().KeyChar;
+                        Console.WriteLine();
+
+                        var node = Find(root, name);
+
+                        if (node == null)
+                            Console.WriteLine($"{name} not found!");
+                        else
+                            Console.WriteLine($"Father: {node.Father.Name}, Mother: {node.Mother.Name}");
+
+                        break;
+                    case 'q':
+                        var treeText = ToString(root);
+                        File.WriteAllText("tree", treeText);      //  <==> File.WriteAllText("tree", ToString(root));
+
+
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
